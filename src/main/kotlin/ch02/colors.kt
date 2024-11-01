e9d45c081aa7b854d75e8f167090e764e55a527c
RodolfoLubeck <rcflubeck@gmail.com>
2024-10-31 22:36:05 -0300

    - when with set collection and default return - actual chapter 2.3.4

--- src/main/kotlin/ch02/colors.kt
+++ src/main/kotlin/ch02/colors.kt
@@ -34,13 +34,32 @@ enum class Color(
 fun measureColor() = Color.ORANGE
 
 fun getWarmthFromSensor(): String {
-    val color = measureColor()
-    return when(color) {
+//    val color = measureColor()
+//    return when(color) {
+//        Color.RED, Color.ORANGE, Color.YELLOW -> "Warm (red = ${color.r})"
+//        Color.GREEN -> "neutral (green = ${color.g})"
+//        Color.BLUE, Color.INDIGO, Color.VIOLET -> "cold (blue = ${color.b})"
+//    }
+    //quando when é usado como uma expressão (o que significa que seu resultado é usado em uma atribuição ou
+    // como um valor de retorno), o compilador força a construção a ser exhaustive . Isso significa que todos os
+    // caminhos possíveis devem retornar um valor.
+
+    return when(val color = measureColor()) {
         Color.RED, Color.ORANGE, Color.YELLOW -> "Warm (red = ${color.r})"
         Color.GREEN -> "neutral (green = ${color.g})"
         Color.BLUE, Color.INDIGO, Color.VIOLET -> "cold (blue = ${color.b})"
     }
 }
+
+//você pode usar qualquer tipo de objeto como uma condição de ramificação.
+fun mix(c1: Color, c2: Color) =
+    when(setOf(c1,c2)){
+        setOf(Color.RED, Color.YELLOW) -> Color.ORANGE
+        setOf(Color.YELLOW, Color.BLUE) -> Color.GREEN
+        setOf(Color.BLUE, Color.VIOLET) -> Color.INDIGO
+        else -> throw Exception("Dirty Color")
+    }
+
 
 fun main(){
     println(Color.BLUE.rgb)
@@ -50,4 +69,6 @@ fun main(){
 
     println(getWarmthFromSensor())
     //warm (red = 255)
+
+    println(mix(Color.BLUE,Color.YELLOW))
 }
